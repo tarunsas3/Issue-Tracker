7bfc430add10c5e8c5f7eb211f173b6fafa0d30a
Tarun Sastry <60286295+tarunsas3@users.noreply.github.com>
2023-07-26 20:07:01 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect('mongodb+srv://Tarun:XFNR9ekJOXixm3MG@cluster0.myvn379.mongodb.net/items?retryWrites=true&w=majority');
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Failed to connect to MongoDB"));
